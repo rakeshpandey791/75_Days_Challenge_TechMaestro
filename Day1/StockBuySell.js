c4e28bd25e38301bd438ece1803cd9d931677c42
rakeshpandey <rakeshpandey791@gmail.com>
2022-03-25 00:07:13 +0530

    Update in Stock buy sell problem

--- Day1/StockBuySell.js
+++ Day1/StockBuySell.js
@@ -1,17 +1,28 @@
+// var maxProfit = function(prices) {
+//     let maxPrft = 0;
+//     for(let i=0; i<prices.length; i++){
+//         const cDayPrice = prices[i];
+//         let maxDiff = 0;
+//         for(let j=i+1; j<prices.length; j++){
+//             const diff = prices[j] - cDayPrice;
+//             if(maxDiff < diff){
+//                 maxDiff = diff;
+//             }
+//         }
+//         if(maxPrft < maxDiff){
+//             maxPrft = maxDiff;
+//         }
+//     }
+//     return maxPrft;
+// };
+
 var maxProfit = function(prices) {
     let maxPrft = 0;
-    for(let i=0; i<prices.length; i++){
-        const cDayPrice = prices[i];
-        let maxDiff = 0;
-        for(let j=i+1; j<prices.length; j++){
-            const diff = prices[j] - cDayPrice;
-            if(maxDiff < diff){
-                maxDiff = diff;
-            }
-        }
-        if(maxPrft < maxDiff){
-            maxPrft = maxDiff;
-        }
+    let buy = prices[0];
+    for(let i=1; i<prices.length; i++){
+        const diff = prices[i] - buy;
+        buy = Math.min(buy, prices[i]);
+        maxPrft = Math.max(diff, maxPrft);
     }
     return maxPrft;
 };
